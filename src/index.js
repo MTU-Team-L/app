86a633de5ae88b40c943990ef62f6abaf8189e78
Max Isom <code@maxisom.me>
2020-04-08 17:54:56 -0500

    Add app icon and splash screen

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './utils/polyfills';
 import 'react-native-gesture-handler';
 import React from 'react';
+import SplashScreen from 'react-native-splash-screen';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
@@ -38,11 +39,14 @@ const DecksStackScreen = () => (
   </DecksStack.Navigator>
 );
 
-export default () => (
-  <NavigationContainer>
-    <Tab.Navigator>
-      <Tab.Screen name="Cards" component={CardsStackScreen}/>
-      <Tab.Screen name="Decks" component={DecksStackScreen}/>
-    </Tab.Navigator>
-  </NavigationContainer>
-);
+export default () => {
+  SplashScreen.hide();
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Cards" component={CardsStackScreen}/>
+        <Tab.Screen name="Decks" component={DecksStackScreen}/>
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+};
